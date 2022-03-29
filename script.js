177c7ae4a16d6989c1ab1fdfd70d5f35737bd745
alsir <41924833+alsir@users.noreply.github.com>
2022-03-29 12:02:17 +0200

    Add files via upload

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 fetch("https://api.adviceslip.com/advice").then((response)=>{
 	  return response.json();  // converting byte data to json
 }).then(data => {
-	$("p").append(data.slip.id);
+	$("span").html(data.slip.id);
 	$("h2").html(data.slip.advice);
 });
 $("button").click(
@@ -9,7 +9,7 @@ $("button").click(
 fetch("https://api.adviceslip.com/advice").then((response)=>{
 	  return response.json();  // converting byte data to json
 }).then(data => {
-	$("p").append(data.slip.id);
+	$("span").html(data.slip.id);
 	$("h2").html(data.slip.advice);
 });
 });
